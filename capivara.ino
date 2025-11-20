7229e67fbfcf43600ea998cb15c75cd1b96ffdb7
joaofdurao <devjoaofelipe.dlc@gmail.com>
2025-11-19 21:18:07 -0300

    Ajuste final de buzzer e extensão de arquivo

--- capivara.ino
+++ capivara.ino
@@ -8,43 +8,52 @@ int vidas = 3;
 int pontuacao = 0;
 bool jogoRodando = false;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
 
   pinMode(pinoBuzzer, OUTPUT);
 
-  for (int i = 0; i < numToupeiras; i++) {
+  for (int i = 0; i < numToupeiras; i++)
+  {
     pinMode(pinosLED[i], OUTPUT);
     pinMode(pinosBotao[i], INPUT);
     digitalWrite(pinosLED[i], LOW);
   }
 
   randomSeed(analogRead(A0));
-  
+
   Serial.println("--- Jogo 'Bata na Toupeira' ---");
   Serial.println("Pressione qualquer botao para iniciar!");
 }
 
-void loop() {
-  if (!jogoRodando) {
-    if (algumBotaoPressionado()) {
+void loop()
+{
+  if (!jogoRodando)
+  {
+    if (algumBotaoPressionado())
+    {
       jogoRodando = true;
+      tone(pinoBuzzer, 2000, 500);
       Serial.println("Jogo comecou! VALENDO!");
       delay(500);
     }
     return;
   }
 
-  if (vidas <= 0) {
+  if (vidas <= 0)
+  {
+    tone(pinoBuzzer, 2000, 500);
     Serial.println("\n--- FIM DE JOGO ---");
     Serial.print("Pontuacao Final: ");
     Serial.println(pontuacao);
     Serial.println("Pressione qualquer botao para reiniciar.");
 
-    while (!algumBotaoPressionado()) {
+    while (!algumBotaoPressionado())
+    {
       piscarTodosLEDs();
     }
-    
+
     vidas = 3;
     pontuacao = 0;
     jogoRodando = false;
@@ -59,27 +68,31 @@ void loop() {
 
   digitalWrite(pinosLED[toupeiraSorteada], HIGH);
   long tempoInicio = millis();
-  
+
   bool acertou = false;
-  
-  while (millis() - tempoInicio < tempoToupeiraAcesa) {
-    if (digitalRead(pinosBotao[toupeiraSorteada]) == HIGH) {
+
+  while (millis() - tempoInicio < tempoToupeiraAcesa)
+  {
+    if (digitalRead(pinosBotao[toupeiraSorteada]) == HIGH)
+    {
       acertou = true;
       break;
     }
   }
-  
+
   digitalWrite(pinosLED[toupeiraSorteada], LOW);
 
-  if (acertou) {
+  if (acertou)
+  {
     pontuacao++;
     Serial.print("ACERTOU! ");
     Serial.print("Pontos: ");
     Serial.println(pontuacao);
-    
-    tone(pinoBuzzer, 2000, 10);
-    
-  } else {
+
+    tone(pinoBuzzer, 2000, 50);
+  }
+  else
+  {
     vidas--;
     Serial.print("ERROU! ");
     Serial.print("Vidas restantes: ");
@@ -89,28 +102,36 @@ void loop() {
   delay(random(500, 1501));
 }
 
-bool algumBotaoPressionado() {
-  for (int i = 0; i < numToupeiras; i++) {
-    if (digitalRead(pinosBotao[i]) == HIGH) {
+bool algumBotaoPressionado()
+{
+  for (int i = 0; i < numToupeiras; i++)
+  {
+    if (digitalRead(pinosBotao[i]) == HIGH)
+    {
       return true;
     }
   }
   return false;
 }
 
-void piscarTodosLEDs() {
-  for (int i = 0; i < numToupeiras; i++) {
+void piscarTodosLEDs()
+{
+  for (int i = 0; i < numToupeiras; i++)
+  {
     digitalWrite(pinosLED[i], HIGH);
   }
   delay(250);
-  for (int i = 0; i < numToupeiras; i++) {
+  for (int i = 0; i < numToupeiras; i++)
+  {
     digitalWrite(pinosLED[i], LOW);
   }
   delay(250);
 }
 
-void apagarTodosLEDs() {
-    for (int i = 0; i < numToupeiras; i++) {
+void apagarTodosLEDs()
+{
+  for (int i = 0; i < numToupeiras; i++)
+  {
     digitalWrite(pinosLED[i], LOW);
   }
 }
